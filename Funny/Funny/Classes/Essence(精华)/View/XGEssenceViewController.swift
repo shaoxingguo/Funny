36b2c8294137c78a6d58f75786ebd4e12812215f
邵兴国 <541145534@qq.com>
2019-03-11 21:01:45 +0800

    搭建应用程序结构

---
+++ Funny/Funny/Classes/Essence(精华)/View/XGEssenceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  XGEssenceViewController.swift
+//  Funny
+//
+//  Created by monkey on 2019/3/11.
+//  Copyright © 2019 itcast. All rights reserved.
+//
+
+import UIKit
+
+class XGEssenceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
