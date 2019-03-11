5bd6661288e1be0bb2b45b249fb9789485148187
邵兴国 <541145534@qq.com>
2019-03-11 20:11:58 +0800

    初始化项目

---
+++ Funny/Funny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Funny
+//
+//  Created by monkey on 2019/3/11.
+//  Copyright © 2019 itcast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
